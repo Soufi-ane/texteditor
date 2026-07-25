56b2c46132c29ebb6b9f124be28c83dc9a354c0f
Soufiane <jabersoufiane605@gmail.com>
2026-07-25 12:12:15 +0100

    fix: forgot this

--- include/draw.h
+++ include/draw.h
@@ -1,7 +1,12 @@
 #ifndef DRAW_H
 #define DRAW_H
+#include <raylib.h>
 #include "buffer.h"
 
 void DrawCursor(Editor* e,int x,int y);
+
+void DrawMenu(Editor * e);
+
+void DrawCurrentMessage(Editor *e);
 
 #endif
