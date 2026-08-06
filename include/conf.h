5c9a720823647a029b9eff331f849655079b6fe8
Soufiane <jabersoufiane605@gmail.com>
2026-08-06 18:38:36 +0100

    fix: lighter font

--- include/conf.h
+++ include/conf.h
@@ -24,11 +24,11 @@
 
 // paths
   
-#define SECONDARY_FONT_PATH "assets/fonts/JetBrainsMonoNF.ttf"
+#define SECONDARY_FONT_PATH "assets/fonts/JetBrainsMono-Regular.ttf"
 #ifdef PROD
-#define FONT_PATH "/usr/local/share/fonts/JetBrainsMonoNF.ttf"
+#define FONT_PATH "/usr/local/share/fonts/JetBrainsMono-Regular.ttf"
 #else 
-#define FONT_PATH "assets/fonts/JetBrainsMonoNF.ttf"
+#define FONT_PATH "assets/fonts/JetBrainsMono-Regular.ttf"
 #endif
 
 #endif
